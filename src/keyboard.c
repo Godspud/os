48dd6e09e2ccdff34f9d38c5cee9b41f5b99e5fe
god_spud <beamongus.yt@gmail.com>
2026-03-12 14:40:33 +0800

    Update: 2026-03-12 14:40

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,8 +1,8 @@
 #include "keyboard.h"
 #include "io.h"
 
-unsigned int keyboard_repeat_delay = 1000;
-unsigned int keyboard_repeat_rate = 500;
+unsigned int keyboard_repeat_delay = 10000;
+unsigned int keyboard_repeat_rate = 5000;
 
 static int repeat_enabled = 1;
 static int last_scancode = 0;
